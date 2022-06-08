b5003b31186d52da49a58d25bc3df0ebfac9da48
codewithsam110g <68802791+codewithsam110g@users.noreply.github.com>
2022-06-08 21:55:13 +0530

    Database
    Basic Database Functionality

--- app/src/main/java/com/rgukt/attend/MainActivity.java
+++ app/src/main/java/com/rgukt/attend/MainActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView tel, eng, mat, phy, chem, it;
-
+    private FirebaseDatabase mRef;
+    private DatabaseReference mDataRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
         phy = findViewById(R.id.txt_phy);
         chem = findViewById(R.id.txt_chem);
         it = findViewById(R.id.txt_it);
+
+        mRef = FirebaseDatabase.getInstance();
+        mDataRef = mRef.getReference("Data");
+
+        UserData data;
+        data = new UserData(10, 8, 8, 8, 8, 8, 8, 8);
+
+        mDataRef.setValue(data);
 
     }
 }
